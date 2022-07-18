2bdfe9e64cc406d6ffd0c7cdcfba0bcd84a92c8f
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2022-07-17 22:56:10 -0300

    Criando um exemplo de crud com os padrões rest.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 const livros = [
   {
     id: 1,
@@ -24,5 +26,33 @@ app.get('/', (req, res) => {
 app.get('/livros', (req, res) => {
   res.status(200).json(livros);
 });
+
+app.get('/livros/:id', (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
+});
+
+app.post('/livros', (req, res) => {
+  livros.push(req.body);
+  res.status(201).send('O livro cadastrado com sucesso');
+});
+
+app.put('/livros/:id', (req, res) => {
+  const index = buscaLivro(req.params.id);
+
+  livros[index].titulo = req.body.titulo;
+  res.status(200).json(livros);
+});
+
+app.delete('/livros/:id', (req, res) => {
+  const { id } = req.params;
+  const index = buscaLivro(id);
+  livros.splice(index, 1);
+  res.send(`Livro ${id} foi removido com sucesso.`);
+});
+
+function buscaLivro(id) {
+  return livros.findIndex((livro) => livro.id == id);
+}
 
 export default app;
